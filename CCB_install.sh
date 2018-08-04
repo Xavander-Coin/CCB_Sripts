78c99f6eadb3560921835a785dbb28474632882e
TFinch <travisfinch01@gmail.com>
2018-08-04 11:23:02 -0400

    Update CCB_install.sh

--- CCB_install.sh
+++ CCB_install.sh
@@ -1,122 +1,122 @@
 #!/bin/bash
 
-TMP_FOLDER=$(mktemp -d)
-CONFIG_FILE='cryptocashback.conf'
-CONFIGFOLDER='/root/.cryptocashback'
-COIN_DAEMON='cryptocashbackd'
-COIN_CLI='cryptocashback-cli'
-COIN_PATH='/usr/local/bin/'
-COIN_REPO='https://github.com/CryptoCashBack-Hub/CCB.git'
-COIN_TGZ=''
-COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
-COIN_NAME='CryptoCashBack'
-COIN_PORT=19551
-RPC_PORT=19552
-SSH_CONFIG="/etc/ssh/sshd_config"
-SSH_PORT=`grep Port $SSH_CONFIG | cut -d " " -f 2`
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=6
+BACKTITLE="CCB Masternode Setup Wizard"
+TITLE="CCB VPS Setup"
+MENU="Choose one of the following options:"
 
-NODEIP=$(curl -s4 icanhazip.com)
+OPTIONS=(1 "Install New VPS Server"
+         2 "Update to new version VPS Server"
+         3 "Start CCB Masternode"
+	 4 "Stop CCB Masternode"
+	 5 "CCB Server Status"
+	 6 "Rebuild CCB Masternode Index")
 
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
+CHOICE=$(whiptail --clear\
+		--backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            echo Starting the install process.
+echo Checking and installing VPS server prerequisites. Please wait.
+echo -e "Checking if swap space is needed."
+PHYMEM=$(free -g|awk '/^Mem:/{print $2}')
+SWAP=$(swapon -s)
+if [[ "$PHYMEM" -lt "2" && -z "$SWAP" ]];
+  then
+    echo -e "${GREEN}Server is running with less than 2G of RAM, creating 2G swap file.${NC}"
+    dd if=/dev/zero of=/swapfile bs=1024 count=2M
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon -a /swapfile
+else
+  echo -e "${GREEN}The server running with at least 2G of RAM, or SWAP exists.${NC}"
+fi
+if [[ $(lsb_release -d) != *16.04* ]]; then
+  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+  exit 1
+fi
+
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}$0 must be run as root.${NC}"
+   exit 1
+fi
+clear
+sudo apt update
+sudo apt-get -y upgrade
+sudo apt-get install git -y
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils -y
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
+sudo apt-get install libboost-all-dev -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sudo apt-get install libminiupnpc-dev -y
+sudo apt-get install libzmq3-dev -y
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
+sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler -y
+clear
+echo VPS Server prerequisites installed.
 
 
-function compile_node() {
-  echo -e "Prepare to compile $COIN_NAME"
-  git clone $COIN_REPO $TMP_FOLDER >/dev/null 2>&1
-  compile_error
-  cd $TMP_FOLDER
-  chmod +x ./autogen.sh 
-  chmod +x ./share/genbuild.sh
-  chmod +x ./src/leveldb/build_detect_platform
-  ./autogen.sh
-  compile_error
-  ./configure
-  compile_error
-  make
-  compile_error
-  make install
-  compile_error
-  strip $COIN_PATH$COIN_DAEMON $COIN_PATH$COIN_CLI
-  cd - >/dev/null 2>&1
-  rm -rf $TMP_FOLDER >/dev/null 2>&1
-  clear
-}
+echo Configuring server firewall.
+sudo apt-get install -y ufw
+sudo ufw allow 19551
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw logging on
+echo "y" | sudo ufw enable
+sudo ufw status
+echo Server firewall configuration completed.
 
-function download_node() {
-  echo -e "Prepare to download $COIN_NAME binaries"
-  cd $TMP_FOLDER
-  wget -q $COIN_TGZ
-  tar xvzf $COIN_ZIP -C /usr/local/bin/
-  compile_error
-  chmod +x $COIN_PATH$COIN_DAEMON $COIN_PATH$COIN_CLI
-  cd - >/dev/null 2>&1
-  rm -r $TMP_FOLDER >/dev/null 2>&1
-  clear
-}
+echo Downloading AquilaX install files.
+wget https://github.com/CryptoCashBack-Hub/CCB/releases/download/V1.0.0.0/CCB-linux.tar.gz
+echo Download complete.
 
-function ask_permission() {
- echo -e "${RED}I trust zoldur and want to use$ $COIN_NAME binaries compiled on his server.${NC}."
- echo -e "Please type ${RED}YES${NC} if you want to use precompiled binaries, or type anything else to compile them on your server"
- read -e ZOLDUR
-}
+echo Installing CCB.
+tar -xvf CCB-linux.tar.gz
+chmod 775 ./cryptocashbackd
+chmod 775 ./cryptocashback-cli
+echo cryptocashback install complete. 
+sudo rm -rf CCB-linux.tar.gz
+clear
 
-function configure_systemd() {
-  cat << EOF > /etc/systemd/system/$COIN_NAME.service
-[Unit]
-Description=$COIN_NAME service
-After=network.target
+echo Now ready to setup AquilaX configuration file.
 
-[Service]
-User=root
-Group=root
+RPCUSER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+RPCPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+VPSIP=$(curl -s4 icanhazip.com)
+echo Please input your private key.
+read GENKEY
 
-Type=forking
-#PIDFile=$CONFIGFOLDER/$COIN_NAME.pid
+mkdir -p /root/.cryptocashback && touch /root/.cryptocashback/cryptocashback.conf
 
-ExecStart=$COIN_PATH$COIN_DAEMON -daemon -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER
-ExecStop=-$COIN_PATH$COIN_CLI -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER stop
-
-Restart=always
-PrivateTmp=true
-TimeoutStopSec=60s
-TimeoutStartSec=10s
-StartLimitInterval=120s
-StartLimitBurst=5
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-  systemctl daemon-reload
-  sleep 3
-  systemctl start $COIN_NAME.service
-  systemctl enable $COIN_NAME.service >/dev/null 2>&1
-
-  if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
-    echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
-    echo -e "${GREEN}systemctl start $COIN_NAME.service"
-    echo -e "systemctl status $COIN_NAME.service"
-    echo -e "less /var/log/syslog${NC}"
-    exit 1
-  fi
-}
-
-
-function create_config() {
-  mkdir $CONFIGFOLDER >/dev/null 2>&1
-  RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
-  RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
-  cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
+cat << EOF > /root/.cryptocashback/cryptocashback.conf
 rpcuser=$RPCUSER
 rpcpassword=$RPCPASSWORD
 rpcallowip=127.0.0.1
-listen=1
 server=1
+listen=1
 daemon=1
-port=$COIN_PORT
+staking=1
+rpcallowip=127.0.0.1
+rpcport=19552
+port=19551
+logtimestamps=1
+maxconnections=256
+masternode=1
+externalip=$VPSIP
+masternodeprivkey=$GENKEY
 addnode=64.110.129.105:19551
 addnode=172.110.10.131:19551
 addnode=172.110.18.12:19551
@@ -140,188 +140,64 @@ addnode=202.182.96.101:19551
 addnode=155.94.164.212:19551
 addnode=173.82.154.110:19551
 addnode=45.77.64.173:19551
+
 EOF
-}
-
-function create_key() {
-  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
-  read -e COINKEY
-  if [[ -z "$COINKEY" ]]; then
-  $COIN_PATH$COIN_DAEMON -daemon
-  sleep 30
-  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
-   echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
-   exit 1
-  fi
-  COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  if [ "$?" -gt "0" ];
-    then
-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
-    sleep 30
-    COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  fi
-  $COIN_PATH$COIN_CLI stop
-fi
 clear
-}
-
-function update_config() {
-  sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
-  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
-logintimestamps=1
-maxconnections=256
-#bind=$NODEIP
-masternode=1
-externalip=$NODEIP:$COIN_PORT
-masternodeprivkey=$COINKEY
-EOF
-}
-
-
-function enable_firewall() {
-  echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
-  ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null
-  ufw allow $SSH_PORT comment "SSH" >/dev/null 2>&1
-  ufw limit $SSH_PORT/tcp >/dev/null 2>&1
-  ufw default allow outgoing >/dev/null 2>&1
-  echo "y" | ufw enable >/dev/null 2>&1
-}
-
-
-
-function get_ip() {
-  declare -a NODE_IPS
-  for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
-  do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
-  done
-
-  if [ ${#NODE_IPS[@]} -gt 1 ]
-    then
-      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
-      INDEX=0
-      for ip in "${NODE_IPS[@]}"
-      do
-        echo ${INDEX} $ip
-        let INDEX=${INDEX}+1
-      done
-      read -e choose_ip
-      NODEIP=${NODE_IPS[$choose_ip]}
-  else
-    NODEIP=${NODE_IPS[0]}
-  fi
-}
-
-
-function compile_error() {
-if [ "$?" -gt "0" ];
- then
-  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
-  exit 1
-fi
-}
-
-
-function checks() {
-if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
-  exit 1
-fi
-
-if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}$0 must be run as root.${NC}"
-   exit 1
-fi
-
-if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMOM" ] ; then
-  echo -e "${RED}$COIN_NAME is already installed.${NC}"
-  exit 1
-fi
-}
-
-function prepare_system() {
-echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
-apt-get update >/dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
-apt install -y software-properties-common >/dev/null 2>&1
-echo -e "${GREEN}Adding bitcoin PPA repository"
-apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
-echo -e "Installing required packages, it may take some time to finish.${NC}"
-apt-get update >/dev/null 2>&1
-apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
-build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
-libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ libzmq5 >/dev/null 2>&1
-if [ "$?" -gt "0" ];
-  then
-    echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
-    echo "apt-get update"
-    echo "apt -y install software-properties-common"
-    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
-    echo "apt-get update"
-    echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
-libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev libzmq5"
- exit 1
-fi
-
+./cryptocashbackd -daemon
+./cryptocashback-cli stop
+./cryptocashbackd -daemon
 clear
-}
+echo cryptocashback configuration file created successfully. 
+echo cryptocashback Server Started Successfully using the command ./Aquilad -daemon
+echo If you get a message asking to rebuild the database, please hit Ctr + C and run ./cryptocashbackd -daemon -reindex
+echo If you still have further issues please reach out to support in our Discord channel. 
+echo Please use the following Private Key when setting up your wallet: $GENKEY
+            ;;
+	    
+    
+        2)
+sudo ./cryptocashback-cli -daemon stop
+echo "! Stopping CCB Daemon !"
 
-function create_swap() {
- echo -e "Checking if swap space is needed."
- PHYMEM=$(free -g|awk '/^Mem:/{print $2}')
- SWAP=$(free -g|awk '/^Swap:/{print $2}')
- if [ "$PHYMEM" -lt "2" ] && [ -n "$SWAP" ]
-  then
-    echo -e "${GREEN}Server is running with less than 2G of RAM without SWAP, creating 2G swap file.${NC}"
-    SWAPFILE=$(mktemp)
-    dd if=/dev/zero of=$SWAPFILE bs=1024 count=2M
-    chmod 600 $SWAPFILE
-    mkswap $SWAPFILE
-    swapon -a $SWAPFILE
- else
-  echo -e "${GREEN}Server running with at least 2G of RAM, no swap needed.${NC}"
- fi
- clear
-}
+echo Configuring server firewall.
+sudo apt-get install -y ufw
+sudo ufw allow 19551
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw logging on
+echo "y" | sudo ufw enable
+sudo ufw status
+echo Server firewall configuration completed.
 
-
-function important_information() {
- echo
- echo -e "================================================================================================================================"
- echo -e "$COIN_NAME Masternode is up and running listening on port ${RED}$COIN_PORT${NC}."
- echo -e "Configuration file is: ${RED}$CONFIGFOLDER/$CONFIG_FILE${NC}"
- echo -e "Start: ${RED}systemctl start $COIN_NAME.service${NC}"
- echo -e "Stop: ${RED}systemctl stop $COIN_NAME.service${NC}"
- echo -e "VPS_IP:PORT ${RED}$NODEIP:$COIN_PORT${NC}"
- echo -e "MASTERNODE PRIVATEKEY is: ${RED}$COINKEY${NC}"
- echo -e "Please check ${RED}$COIN_NAME${NC} is running with the following command: ${RED}systemctl status $COIN_NAME.service${NC}"
- echo -e "================================================================================================================================"
-}
-
-function setup_node() {
-  get_ip
-  create_config
-  create_key
-  update_config
-  enable_firewall
-  important_information
-  configure_systemd
-}
+echo "! Removing CCB !"
+sudo rm -rf CCB_install.sh
+sudo rm -rf cryptocashbackd
+sudo rm -rf cryptocashback-cli
+sudo rm -rf cryptocashback-qt
 
 
-##### Main #####
-clear
 
-checks
-prepare_system
-#ask_permission
-#if [[ "$ZOLDUR" == "YES" ]]; then
-#  download_node
-#else
-  create_swap
-  compile_node
-#fi
-setup_node
+wget https://github.com/CryptoCashBack-Hub/CCB/releases/download/V1.0.0.0/CCB-linux.tar.gz
+echo Download complete.
+echo Installing CCB.
+tar -xvf CCB-linux.tar.gz
+chmod 775 ./cryptocashbackd
+chmod 775 ./cryptocashback-cli
+echo AquilaX install complete. 
+sudo rm -rf CCB-linux.tar.gz
+
+            ;;
+        3)
+            ./cryptocashbackd -daemon
+		echo "If you get a message asking to rebuild the database, please hit Ctr + C and rebuild Aquila Index. (Option 6)"
+            ;;
+	4)
+            ./cryptocashback-cli stop
+            ;;
+	5)
+	    ./cryptocashback-cli getinfo
+	    ;;
+        6)
+	     ./cryptocashbackd -daemon -reindex
+            ;;
+esac
