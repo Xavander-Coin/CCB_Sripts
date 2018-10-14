9aead03fbc338d3a0f98c4af603355f8c3cd4ac6
TFinch <travisfinch01@gmail.com>
2018-10-14 18:16:56 -0400

    Update CCB_install.sh

--- CCB_install.sh
+++ CCB_install.sh
@@ -80,7 +80,7 @@ sudo ufw status
 echo Server firewall configuration completed.
 
 echo Downloading CCBC install files.
-wget https://github.com/CryptoCashBack-Hub/CCBC/releases/download/v1.0.0.4/CCBC-linux.tar.gz
+wget https://github.com/CryptoCashBack-Hub/CCBC/releases/download/v1.0.0.5/CCBC-linux.tar.gz
 echo Download complete.
 
 echo Installing CCBC.
@@ -158,7 +158,7 @@ echo "! Removing Concierge !"
 sudo rm -rf CCBC-linux.tar.gz
 
 
-wget https://github.com/CryptoCashBack-Hub/CCBC/releases/download/v1.0.0.4/CCBC-linux.tar.gz
+wget https://github.com/CryptoCashBack-Hub/CCBC/releases/download/v1.0.0.5/CCBC-linux.tar.gz
 echo Download complete.
 echo Installing CCBC.
 tar -xvf CCBC-linux.tar.gz
